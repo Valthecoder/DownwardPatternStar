05b04a73683ea17b342287fa00ef74f51c7aae6b
ValtheCoder <keshia.kdf@gmail.com>
2024-01-25 17:27:01 +0800

    Star Downward Pattern Asterisk

--- DownwardPatternStar.py
+++ DownwardPatternStar.py
@@ -1,3 +1,8 @@
 # Number of rows
+star_row = 5
 # Create nested loops
+for star in range(star_row + 1, 0, -1):
+    for asterisk in range(0, star - 1):
+        print("*", end=' ')
 # Print
+    print(" ")
